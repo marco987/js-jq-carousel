ccfe23715c3802c02e83338a8c1351c7b0bd3715
marco987 <pacemarco87@gmail.com>
2019-07-21 16:36:22 +0200

    Funzione next e prev

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-//
+//Ricreare il carousel (o slideshow se preferite).
+// Quindi frecce next prev + pallini.
 
 $(document).ready(function(){
 
-// Scrivere codice qui
+  $(".next").click(nextimg);
+
+  function nextimg() {
+    var imgActive = $(".img img.active");
+    imgActive.removeClass("active");
+
+    if(imgActive.hasClass("last")) {
+      $(".first").addClass("active");
+    } else {
+      imgActive.next("img").addClass("active");
+    }
+  }
+
+  $(".prev").click(previmg);
+
+  function previmg() {
+    var imgActive = $(".img img.active");
+    imgActive.removeClass("active");
+
+    if(imgActive.hasClass("first")) {
+      $(".last").addClass("active");
+    } else {
+      imgActive.prev("img").addClass("active");
+    }
+  }
 
 
 
