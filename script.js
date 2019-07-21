1a18803cb74fe3a4e86535a3cdaaf803a5198f05
marco987 <pacemarco87@gmail.com>
2019-07-21 16:52:28 +0200

    Funzione pallini

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ $(document).ready(function(){
 
   function nextimg() {
     var imgActive = $(".img img.active");
-    imgActive.removeClass("active");
+    var pallActive = $(".pallini span.active");
 
-    if(imgActive.hasClass("last")) {
+    imgActive.removeClass("active");
+    pallActive.removeClass("active");
+
+    if(imgActive.hasClass("last") && pallActive.hasClass("last")) {
       $(".first").addClass("active");
+      $(".pallini span.first").addClass("active");
     } else {
       imgActive.next("img").addClass("active");
+      pallActive.next("span").addClass("active");
     }
   }
 
@@ -20,12 +25,17 @@ $(document).ready(function(){
 
   function previmg() {
     var imgActive = $(".img img.active");
+    var pallActive = $(".pallini span.active");
+
     imgActive.removeClass("active");
+    pallActive.removeClass("active");
 
     if(imgActive.hasClass("first")) {
       $(".last").addClass("active");
+      $(".pallini span.last").addClass("active");
     } else {
       imgActive.prev("img").addClass("active");
+      pallActive.prev("span").addClass("active");
     }
   }
 
