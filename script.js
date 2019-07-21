634e795128f2fd2cb64d23cd3b388038cb0eeef6
marco987 <pacemarco87@gmail.com>
2019-07-21 17:04:57 +0200

    Funzioni Versione compatta

--- script.js
+++ script.js
@@ -3,39 +3,68 @@
 
 $(document).ready(function(){
 
+  // // Richiamo e dichiaro funzione next + pallini
+  // $(".next").click(nextimg);
+  //
+  // function nextimg() {
+  //   var imgActive = $(".img img.active");
+  //   var pallActive = $(".pallini span.active");
+  //
+  //   imgActive.removeClass("active");
+  //   pallActive.removeClass("active");
+  //
+  //   if(imgActive.hasClass("last") && pallActive.hasClass("last")) {
+  //     $(".first").addClass("active");
+  //     $(".pallini span.first").addClass("active");
+  //   } else {
+  //     imgActive.next("img").addClass("active");
+  //     pallActive.next("span").addClass("active");
+  //   }
+  // }
+  //
+  // // Richiamo e dichiaro funzione prev + pallini
+  // $(".prev").click(previmg);
+  //
+  // function previmg() {
+  //   var imgActive = $(".img img.active");
+  //   var pallActive = $(".pallini span.active");
+  //
+  //   imgActive.removeClass("active");
+  //   pallActive.removeClass("active");
+  //
+  //   if(imgActive.hasClass("first")) {
+  //     $(".last").addClass("active");
+  //     $(".pallini span.last").addClass("active");
+  //   } else {
+  //     imgActive.prev("img").addClass("active");
+  //     pallActive.prev("span").addClass("active");
+  //   }
+  // }
+
+  // VERSIONE COMPATTA
+  // Richiamo e dichiaro funzione next + pallini
   $(".next").click(nextimg);
 
   function nextimg() {
-    var imgActive = $(".img img.active");
-    var pallActive = $(".pallini span.active");
-
-    imgActive.removeClass("active");
-    pallActive.removeClass("active");
-
-    if(imgActive.hasClass("last") && pallActive.hasClass("last")) {
-      $(".first").addClass("active");
-      $(".pallini span.first").addClass("active");
+    var active = $(".img img.active, .pallini span.active");
+    active.removeClass("active");
+    if(active.hasClass("last")) {
+      $(".first, .pallini span.first").addClass("active");
     } else {
-      imgActive.next("img").addClass("active");
-      pallActive.next("span").addClass("active");
+      active.next("img, span").addClass("active");
     }
   }
 
+  // Richiamo e dichiaro funzione prev + pallini
   $(".prev").click(previmg);
 
   function previmg() {
-    var imgActive = $(".img img.active");
-    var pallActive = $(".pallini span.active");
-
-    imgActive.removeClass("active");
-    pallActive.removeClass("active");
-
-    if(imgActive.hasClass("first")) {
-      $(".last").addClass("active");
-      $(".pallini span.last").addClass("active");
+    var active = $(".img img.active, .pallini span.active")
+    active.removeClass("active");
+    if(active.hasClass("first")) {
+      $(".last, .pallini span.last").addClass("active");
     } else {
-      imgActive.prev("img").addClass("active");
-      pallActive.prev("span").addClass("active");
+      active.prev("img, span").addClass("active");
     }
   }
 
